21e096a13d9f71ffb65fa52855b415b179e70609
Ovv <contact@ovv.wtf>
2017-12-03 22:23:18 +0100

    initial commit

---
+++ templates/opt/nextcloud/config/custom.config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+    'trusted_domains' =>
+        array (
+            'localhost',
+            '{{ nextcloud_domain }}',
+        ),
+    'overwrite.cli.url' => 'https://{{ nextcloud_domain }}',
+    'overwritehost' => '{{ nextcloud_domain }}',
+    'overwriteprotocol' => 'https',
+    'memcache.local' => '\OC\Memcache\APCu',
+    'logtimezone' => 'UTC',
+    'mail_from_address' => 'noreply',
+    'mail_smtpmode' => 'smtp    ',
+    'mail_domain' => '{{ nextcloud_email_domain }}',
+    'mail_smtphost' => '192.168.0.1',
+    'mail_smtpport' => '25',
+    'mail_smtpauthtype' => 'PLAIN',
+    'default_language' => 'en',
+    'defaultapp' => 'files',
+    'trashbin_retention_obligation' => 'auto',
+    'auth.bruteforce.protection.enabled' => true,
+);
